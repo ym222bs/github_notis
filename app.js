daf45da2bbf27fb6424c5df822e64ae9840fb773
Yasmin <ym222bs@student.lnu.se>
2020-03-02 20:43:41 +0100

    heroku test

--- app.js
+++ app.js
@@ -41,13 +41,12 @@ const port = process.env.PORT || 8000
 
 // If application is running from Heroku then build React to 
 // static files and serve from relative path:
-
-	app.use(express.static(path.join(__dirname, 'build')))
-
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 	})
-
+}
 
 app.listen(port, () => {
 	console.log(`Hello port ${port}.`)
