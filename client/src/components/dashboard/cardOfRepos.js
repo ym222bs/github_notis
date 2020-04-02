e04c17e04eb26a75742e341550bc250475aa9f50
Yasmin <ym222bs@student.lnu.se>
2020-04-02 20:33:25 +0200

    added scroll

--- client/src/components/dashboard/cardOfRepos.js
+++ client/src/components/dashboard/cardOfRepos.js
@@ -8,27 +8,25 @@ const CardOfRepos = ({ repos }) => {
 
     return (
       <Fragment>
-        <div className='overflow-scroll'>
-          {
-            newArrayOfObjects.map(property =>
-              <div className='list-group' key={count++}>
-                <div className='list-group-item' style={{ marginBottom: '1rem' }}>
-                  <div
-                    style={{ fontSize: 'medium' }}>
-                    {property.full_name}
-                  </div>
-                  <a
-                    href={property.html_url}
-                    style={{ fontSize: 'small' }}
-                  >
-                    {property.html_url}
-                  </a>
-
+        {
+          newArrayOfObjects.slice(0, 8).map(property =>
+            <div className='list-group' key={count++}>
+              <div className='list-group-item' style={{ marginBottom: '1rem' }}>
+                <div
+                  style={{ fontSize: 'medium' }}>
+                  {property.full_name}
                 </div>
+                <a
+                  href={property.html_url}
+                  style={{ fontSize: 'small' }}
+                >
+                  {property.html_url}
+                </a>
+
               </div>
-            )
-          }
-        </div>
+            </div>
+          )
+        }
       </Fragment>
     )
   } else {
