d21910d1c3bddaafcb129e544d706b7b2be20c49
Yasmin <ym222bs@student.lnu.se>
2020-05-13 21:53:35 +0200

    added build, testing

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use(passport.session())
 app.use(allowHeaders)
 
 
-
 app.use(cors())
 
 // Initialize MongoDB
@@ -50,12 +49,14 @@ app.get('/', (req, res) => {
   res.send('Home')
 })
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+
+app.use('*', express.static(path.join(__dirname, "client", "build")))
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'))
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   })
+// }
 
 const port = process.env.PORT || 8000
 
