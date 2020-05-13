375e1b3ceeeff95ce44efd5c9e565e21cc04ea71
Yasmin <ym222bs@student.lnu.se>
2020-05-13 22:02:16 +0200

    ss

--- app.js
+++ app.js
@@ -50,13 +50,9 @@ app.get('/', (req, res) => {
 })
 
 
-app.use('*', express.static(path.join(__dirname, "client", "build")))
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'))
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   })
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
 
 const port = process.env.PORT || 8000
 
