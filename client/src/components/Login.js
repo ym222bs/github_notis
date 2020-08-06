18e6efe7b29497b18cdee13de27fc4d3aaab84a1
Yasmin <ym222bs@student.lnu.se>
2020-08-06 22:31:03 +0200

    changed css

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,51 +1,73 @@
 import React from 'react'
 import githubIcon from '../images/github.png'
-import Mac from './Mac.js'
 
 const github = {
-  name: 'Github',
-  img: githubIcon,
-  href: '/auth/github',
-  color: '#24292e'
+    name: 'Github',
+    img: githubIcon,
+    href: '/auth/github',
+    color: '#24292e',
 }
 
 const Home = () => {
-  return (
-    <div>
-      <div
-        className='page'
-        style={{ textAlign: 'center' }}
-      >
-        <p
-          className='page-title'>
-          Github Notifications
-        </p>
-        <Mac />
-        <div
-          className='card-g'
-          onClick={() => window.location = github.href}
-          style={{
-            border: '1xp solid',
-            borderRadius: 2
-          }}
-        >
-          <div>
-            <p>
-              {github.name}
-            </p>
-          </div>
-          <div
-            style={{
-              minHeight: 100,
-              minWidth: 100,
-              background: `url('${github.img}') no-repeat center center / 50% ${github.color}`
-            }}
-          >
-          </div>
+    return (
+        <div>
+            <div className='page'>
+                <p className='page-title' style={{ textAlign: 'left' }}>
+                    + GithubNotis
+                </p>
+                <div
+                    className='page-info jumbotron'
+                    style={{ padding: '5rem' }}
+                >
+                    <div>
+                        <p className='lead' style={{ fontWeight: 'bold' }}>
+                            Keep track, get notified !
+                        </p>
+                        <p>
+                            GithubNotis lets you customize what type of event or
+                            activity you would like to get notified by, on any
+                            of your organizations.
+                        </p>
+                        <p>
+                            You can either login with your Github account OR use{' '}
+                            <br />
+                            <strong>username: just-check1ng </strong> and
+                            <br />
+                            <strong>password: xyz123!"#</strong> to login with a
+                            test account and check it out.
+                        </p>
+                    </div>
+
+                    <div
+                        className='card-g'
+                        onClick={() => (window.location = github.href)}
+                        style={{
+                            border: '1xp solid',
+                        }}
+                    >
+                        <div>
+                            <p
+                                style={{
+                                    textAlign: 'center',
+                                    fontWeight: 'bold',
+                                    color: 'white',
+                                }}
+                            >
+                                {github.name}
+                            </p>
+                        </div>
+                        <div
+                            style={{
+                                minHeight: 100,
+                                minWidth: '100%',
+                                background: `url('${github.img}') no-repeat center center / 50% ${github.color}`,
+                            }}
+                        ></div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Home
--- client/src/components/dashboard/Profile.js
+++ client/src/components/dashboard/Profile.js
@@ -3,66 +3,62 @@ import _ from 'lodash'
 import Content from './Content.js'
 import UserProvider from '../../contexts/UserProvider.jsx'
 
-const loginMsg = 'You have to log in to see info! 🤖 '
+const loginMsg = 'You have to login to see your info! 🤖 '
 const description = 'Your notification app for the good old GitHub..'
 
-
 const Profile = () => {
-  const userData = useContext(UserProvider.context)
-  const { avatar_url } = userData
-  const text = _.isEmpty(userData) ? loginMsg : 'Explore Your Orgs 😎 💬 '
-  const image = !_.isEmpty(userData) ? avatar_url : 'Image'
+    const userData = useContext(UserProvider.context)
+    const { avatar_url } = userData
+    const text = _.isEmpty(userData) ? loginMsg : 'Explore Your Orgs 😎 💬 '
+    const image = !_.isEmpty(userData) ? avatar_url : 'Image'
 
-  return (
-    <div className='jumbotron container mt-5'>
-      <h2 className='display-4'>
-        {text}
-      </h2>
-      {
-        _.isEmpty(userData) &&
-        <a
-          className='btn btn-outline-dark'
-          href={'/'}
-          style={{ float: "right" }}>
-          Login
-				</a>
-      }
-      {
-        !_.isEmpty(userData) &&
-        <Fragment>
-          <a
-            href={'/auth/logout'}
-            style={{ float: "right" }}
-            className='btn btn-outline-dark'
-          >
-            Logout
-					</a>
-          <a
-            href={'/profile'}
-            style={{ float: 'right', marginRight: '5px' }}
-            className='btn btn-outline-info'
-          >
-            Home
-					</a>
-          <div className='lead'>
-            {description}
+    return (
+        <div className='jumbotron container mt-5'>
+            <h2 className='display-4'>{text}</h2>
+            {_.isEmpty(userData) && (
+                <a
+                    className='btn btn-outline-dark'
+                    href={'/'}
+                    style={{ float: 'right' }}
+                >
+                    Login
+                </a>
+            )}
+            {!_.isEmpty(userData) && (
+                <Fragment>
+                    <a
+                        href={'/auth/logout'}
+                        style={{ float: 'right' }}
+                        className='btn btn-outline-dark'
+                    >
+                        Logout
+                    </a>
+                    <a
+                        href={'/profile'}
+                        style={{ float: 'right', marginRight: '5px' }}
+                        className='btn btn-outline-info'
+                    >
+                        Home
+                    </a>
+                    <div className='lead'>
+                        {description}
+                        <hr
+                            className='my-4'
+                            style={{
+                                backgroundColor: 'white',
+                                height: 2,
+                            }}
+                        />
+                        <div className='gridparent'>
+                            <Content avatar={image} />
+                        </div>
+                    </div>
 
-            <hr className='my-4' style={{
-              backgroundColor: 'white',
-              height: 2
-            }} />
-
-            <div className='gridparent'>
-              <Content avatar={image} />
-            </div>
-          </div>
-
-          <div style={{ marginBottom: 20 }} />
-        </Fragment>
-      }
-    </div>
-  )
+                    <div style={{ marginBottom: 20 }} />
+                </Fragment>
+            )}
+        </div>
+    )
 }
-
 
 export default Profile
