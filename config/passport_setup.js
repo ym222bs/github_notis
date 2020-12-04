b4ed00e3462558ec245fb31e7fc166f3febb7ac7
Yasmin <ym222bs@student.lnu.se>
2020-12-04 10:52:09 +0100

    Update passport_setup.js

--- config/passport_setup.js
+++ config/passport_setup.js
@@ -26,23 +26,6 @@ passport.use(new GitHubStrategy({
 },
   async (token, refreshToken, profile, done) => {
     try {
-      // const githubID = profile._json.id
-      // const user = await User.findOne({ git_id: githubID })
-      // if (user) {
-      //   const query = {}
-      //   query.token = token
-      //   await User.updateOne({ git_id: githubID }, { $set: query })
-      //   console.log('user already exists')
-      // } else {
-      //   const newUser = new User({
-      //     name: profile._json.name,
-      //     username: profile._json.login,
-      //     git_id: profile._json.id,
-      //     avatar_url: profile._json.avatar_url,
-      //     token: token
-      //   })
-      //   const saveduser = await newUser.save()
-      // }
       saveProfileToDB(token, profile._json)   // Save new profile to database
       return done(null, { ...profile._json })
     } catch (error) {
